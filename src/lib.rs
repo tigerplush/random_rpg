83e82189d73f5c0c8912452cda5ee2540b7830ac
tigerplush <64077786+tigerplush@users.noreply.github.com>
2024-08-02 11:42:25 +0200

    added optional seed

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,14 @@ use std::{
 use rand::{thread_rng, RngCore};
 use serde::Serialize;
 
-pub fn init(name: Option<&String>) -> Result<(), Box<dyn Error>> {
+pub fn init(name: Option<&String>, seed: Option<&u32>) -> Result<(), Box<dyn Error>> {
     let path = Path::new("./").join(name.unwrap_or(&String::from("unnamed_world")));
     if !Path::exists(&path) {
         fs::create_dir(&path)?;
     }
     let settings_path = Path::new(&path).join("world_settings.yml");
     let file = File::create(settings_path)?;
-    serde_yaml::to_writer(file, &WorldSettings::default())?;
+    serde_yaml::to_writer(file, &WorldSettings::from(seed))?;
     Ok(())
 }
 
@@ -27,6 +27,15 @@ impl Default for WorldSettings {
     fn default() -> Self {
         WorldSettings {
             seed: thread_rng().next_u32(),
+        }
+    }
+}
+
+impl From<Option<&u32>> for WorldSettings {
+    fn from(value: Option<&u32>) -> Self {
+        match value {
+            Some(seed) => WorldSettings { seed: *seed },
+            None => WorldSettings::default(),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command};
+use clap::{value_parser, Arg, Command};
 use random_rpg::init;
 
 fn main() {
@@ -6,14 +6,21 @@ fn main() {
         .subcommand(
             Command::new("init")
                 .about("initialises a new world")
-                .arg(Arg::new("name").short('n').long("name")),
+                .arg(Arg::new("name").short('n').long("name"))
+                .arg(
+                    Arg::new("seed")
+                        .short('s')
+                        .long("seed")
+                        .value_parser(value_parser!(u32)),
+                ),
         )
         .get_matches();
 
     match matches.subcommand() {
         Some(("init", init_matches)) => {
             let name = init_matches.get_one::<String>("name");
-            match init(name) {
+            let seed = init_matches.get_one::<u32>("seed");
+            match init(name, seed) {
                 Ok(_) => (),
                 Err(error) => panic!("Problem creating new world: {error:?}"),
             };
